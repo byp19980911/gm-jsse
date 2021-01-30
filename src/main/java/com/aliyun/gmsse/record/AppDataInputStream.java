a05e4b1f8229ae5522400d8d86a32921d63d78f4
byp19980911 <byp19980911@163.com>
2021-01-30 13:51:07 +0800

    Update AppDataInputStream.java
    修改一个tls包只能够读取默认字节(8192)的问题

--- src/main/java/com/aliyun/gmsse/record/AppDataInputStream.java
+++ src/main/java/com/aliyun/gmsse/record/AppDataInputStream.java
@@ -1,17 +1,23 @@
 package com.aliyun.gmsse.record;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import com.aliyun.gmsse.Record;
 import com.aliyun.gmsse.RecordStream;
 
-public class AppDataInputStream extends InputStream {
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 
+public class AppDataInputStream extends InputStream
+{
     private RecordStream recordStream;
+    private ByteBuffer buffer;
+    private boolean appDataIsAvailable;
 
-    public AppDataInputStream(RecordStream recordStream) {
+    public AppDataInputStream(RecordStream recordStream)
+    {
         this.recordStream = recordStream;
+        buffer = ByteBuffer.allocate(4096);
+        appDataIsAvailable = false;
     }
 
     @Override
@@ -23,9 +29,50 @@ public class AppDataInputStream extends InputStream {
 
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
-        Record record = recordStream.read(true);
-        int length = Math.min(record.fragment.length, len);
-        System.arraycopy(record.fragment, 0, b, off, length);
-        return length;
+        int remains = available();
+        if (remains > 0) {
+            int howmany = Math.min(remains, len);
+            buffer.get(b, off, howmany);
+
+            return howmany;
+        }
+
+        appDataIsAvailable = false;
+        try
+        {
+            buffer.clear();
+            Record record = recordStream.read(true);
+            int packetLen = record.fragment.length;
+
+            if (packetLen > buffer.remaining()) {
+                buffer = ByteBuffer.allocate(packetLen);
+            }
+
+            buffer.put(record.fragment, 0, packetLen);
+            buffer.flip();
+            appDataIsAvailable = true;
+
+            int howmany = Math.min(len, packetLen);
+            buffer.get(b, off, howmany);
+            return howmany;
+        } catch (Exception e) {
+            return -1;
+        }
     }
+
+    public int available() throws IOException {
+        if ((!appDataIsAvailable)) {
+            return 0;
+        }
+
+        return buffer.remaining();
+    }
+
+//    @Override
+//    public int read(byte[] b, int off, int len) throws IOException {
+//        Record record = recordStream.read(true);
+//        int length = Math.min(record.fragment.length, len);
+//        System.arraycopy(record.fragment, 0, b, off, length);
+//        return length;
+//    }
 }
